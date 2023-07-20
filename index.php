50b6cd4056bfbd1c7f82871b1f0e4c1276eded9f
Nutxu <138751701+Nattachai928@users.noreply.github.com>
2023-07-20 15:26:26 +0700

    edit index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php echo "MR.NATTACHAI SROISUNGNAE" ; ?>
 <?php echo "STUDENT : 6411130017 " ; ?>
 <?php echo "TEL : 5112336555788 " ; ?>
+
+<?php echo "TEL : 45454545 " ; ?>
+<?php echo " whole world broken" ; ?>
